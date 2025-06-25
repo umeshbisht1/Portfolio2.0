e7b1c863afa985e22ec2fdae0d6247f518417434
umesh bisht1 <umeshbisht792@gmail.com>
2025-06-25 20:05:22 +0530

    images updated

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="/public/images/cropped-image-dt77-1.png"
+        src="/images/cropped-image-dt77-1.png"
         alt="Umesh Singh"
         className="max-h-[450px] w-auto"
       />
